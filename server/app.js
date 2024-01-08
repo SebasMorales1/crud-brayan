1f6cf5dc96ffcbd4f1c5c0b081716325e818f28c
Sebastian Morales <sebasmorales05col@gmail.com>
2024-01-08 11:06:47 -0500

    [server] Add cors. [client] add structure and responsive design

--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const app = express()
 const useApp = require("./routes/routes.js")
 const bodyParser = require("body-parser")
 const DBconnection= require("./config/db.js")
+const cors = require('cors')
+
 const PORT = 3000
 DBconnection()
+
+app.use(cors())
 app.use(bodyParser.json())
 app.use(useApp)
 
